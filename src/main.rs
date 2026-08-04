b4d6b2714972ff1f98e0b8c0c729c71c8823de37
qqqqb4 <fmitb3334@gmail.com>
2026-08-04 20:17:02 +0300

    Some things were done. Basic config creations and validating

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,196 @@
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::mem::swap;
+
+use directories::ProjectDirs;
 use eframe::*;
+use serde::{Deserialize, Serialize};
 
 const APP_NAME: &str = "TWR";
+const BACKGROUND: egui::Color32 = egui::Color32::TRANSPARENT;
+
+#[derive(Serialize, Deserialize)]
+struct AppConfig {
+    _api_key: String,
+    _message_type: String,    // Open-AI, Anthropic etc. Needs to be enum
+    _input_language: String,  // Also should be enum (for language codes)
+    _output_language: String, // ^
+    _default: bool,
+}
+
+#[allow(clippy::derivable_impls)]
+impl Default for AppConfig {
+    fn default() -> Self {
+        Self {
+            _api_key: String::new(),
+            _message_type: String::new(),
+            _input_language: "Auto".to_string(),
+            _output_language: "English".to_string(),
+            _default: true,
+        }
+    }
+}
+
+impl AppConfig {
+    fn load(_path: std::path::PathBuf) -> Self {
+        Self {
+            _api_key: String::new(),
+            _message_type: String::new(),
+            _input_language: "Auto".to_string(),
+            _output_language: "English".to_string(),
+            _default: true,
+        }
+    } // TODO
+}
 
 struct TrwApp {
+    _name: String,
     input: String,
     output: String,
-    _name: String,
+    input_language: String,
+    output_language: String,
+    _have_config: bool,
 }
 
 impl Default for TrwApp {
     fn default() -> Self {
-        return Self {
+        Self {
             _name: APP_NAME.to_string(),
             input: String::new(),
             output: String::new(),
-        };
+            input_language: "Auto".to_string(),
+            output_language: "English".to_string(),
+            _have_config: true,
+        }
+    }
+}
+
+impl TrwApp {
+    fn new(config: Option<AppConfig>) -> Self {
+        match config {
+            Some(config) => Self {
+                _name: APP_NAME.to_string(),
+                input: String::new(),
+                output: String::new(),
+                input_language: config._input_language,
+                output_language: config._output_language,
+                _have_config: true,
+            },
+            None => Self::default(),
+        }
     }
 }
 
 impl eframe::App for TrwApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut Frame) {
-        egui::CentralPanel::default()
-            .frame(egui::Frame::default().fill(egui::Color32::TRANSPARENT))
+        egui::Panel::left("left_panel")
+            .exact_size(450.0)
+            .show_separator_line(false)
+            .resizable(false)
+            .frame(egui::Frame::default().fill(BACKGROUND))
             .show(ui, |ui| {
-                ui.horizontal_centered(|ui| {
-                    ui.vertical(|ui| {
-                        ui.label("Sample label");
-                        let response =
-                            ui.add(egui::TextEdit::multiline(&mut self.input).desired_width(300.0));
-                        if response.changed() {
-                            self.output = self.input.clone();
-                        }
-                    });
-                    ui.vertical(|ui| {
-                        let _ = ui.button("Button");
-                    });
-                    ui.vertical(|ui| {
-                        ui.label("output");
-                        ui.add(egui::TextEdit::multiline(&mut self.output).desired_width(300.0));
-                    })
+                ui.vertical_centered_justified(|ui| {
+                    let input_lang = self.input_language.as_str();
+                    let _button = ui.button(format!("Input ({input_lang})"));
+                    let response = ui.add(
+                        egui::TextEdit::multiline(&mut self.input)
+                            .hint_text("Input")
+                            .desired_rows(16),
+                    );
+                    if response.changed() {
+                        self.output = self.input.clone();
+                    }
+                });
+            });
+
+        egui::Panel::right("right_panel")
+            .exact_size(450.0)
+            .show_separator_line(false)
+            .resizable(false)
+            .frame(egui::Frame::default().fill(BACKGROUND))
+            .show(ui, |ui| {
+                ui.vertical_centered_justified(|ui| {
+                    let output_lang = self.output_language.as_str();
+                    let _button = ui.button(format!("Output ({output_lang})"));
+                    ui.add(
+                        egui::TextEdit::multiline(&mut self.output)
+                            .hint_text("Output")
+                            .desired_rows(16)
+                            .interactive(false),
+                    );
                 })
             });
+
+        egui::CentralPanel::default()
+            .frame(egui::Frame::default().fill(BACKGROUND))
+            .show(ui, |ui| {
+                ui.vertical_centered(|ui| {
+                    let response = ui.button("<->");
+                    if response.clicked() && !(self.input_language == "Auto") {
+                        swap(&mut self.input_language, &mut self.output_language);
+                    }
+                });
+            });
     }
-    fn logic(&mut self, ctx: &egui::Context, frame: &mut Frame) {
-        ctx.set_pixels_per_point(1.5);
-        ctx.is_pointer_over_egui();
-        println!("{}", ctx.viewport_rect());
+}
+
+fn validate_config_location() -> Result<std::path::PathBuf, io::Error> {
+    let path = ProjectDirs::from("", "", "trw")
+        .unwrap() // maybe custom paths TODO???
+        .config_dir()
+        .to_path_buf();
+
+    fs::create_dir_all(&path)?;
+    let config_path = path.join("config.toml");
+    if !config_path.exists() {
+        File::create_new(&config_path)?;
+        fs::write(
+            &config_path,
+            toml::to_string_pretty(&AppConfig::default()).unwrap(),
+        )?;
     }
+    Ok(config_path)
+}
+
+fn validate_config(config_path: std::path::PathBuf) -> AppConfig {
+    let config_string = fs::read_to_string(config_path);
+    toml::from_str(config_string.unwrap().as_str()).unwrap_or_else(|e| {
+        println!("{}", e);
+        AppConfig::default()
+    })
+}
+
+fn load_config() -> AppConfig {
+    match validate_config_location() {
+        Ok(res) => return validate_config(res),
+        Err(e) => println!("Error: {}", e),
+    }
+    AppConfig::default()
 }
 
 fn main() -> eframe::Result {
+    let config = load_config();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_transparent(true)
+            .with_inner_size([1500.0, 400.0])
             .with_decorations(false)
-            .with_resizable(false)
-            .with_fullscreen(true),
+            .with_transparent(true)
+            .with_always_on_top()
+            .with_resizable(false),
         ..Default::default()
     };
 
-    return eframe::run_native(
+    eframe::run_native(
         APP_NAME,
         options,
-        Box::new(|_cc| Ok(Box::new(TrwApp::default()))),
-    );
+        Box::new(|cc| {
+            cc.egui_ctx.set_pixels_per_point(1.5);
+            Ok(Box::new(TrwApp::new(Some(config))))
+        }),
+    )
 }
+
 // TODO
-// Scaling of the ui elements (and window size) are stupid and ugly.
+// Scaling of the ui elements (and window size) are done stupid and ugly.
+// Optional config fields (fallback to default)
